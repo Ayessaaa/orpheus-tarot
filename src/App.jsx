40cd89516a24474776efeda78a8d6684d24ac5dd
Ayessa <ayessamaedeguzman78@gmail.com>
2025-04-08 21:28:28 +0800

    made it responsive for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -33,15 +33,12 @@ const cardsObject = [
   },
 ];
 
-const API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
-
 const prompt =
   "You are oracle orpheus for tarot reading. Orpheus is a dinosaur so hes kinda silly and dumb and will mispell things and will make you do stupid things. I ask this question to you as orpheus and got this 3 cards randomly: ";
 const prompt2 =
   ", make up some kind of reading based of this and would you respond to the users question: ";
 const prompt3 =
   ". make it straight to the point, orpheus doesn't care about me he is silly and dumb and might mispell things and not afraid to tell what he wants he speaks like a toddler. dont make it too long ";
-
 
 const getOpenAIResponse = async (prompt) => {
   try {
@@ -53,11 +50,10 @@ const getOpenAIResponse = async (prompt) => {
   }
 };
 
-
 function App() {
   const [isLanding, setIsLanding] = useState(true);
   return (
-    <div className="bg-gradient-to-b from-[#121031] from-50% to-[#3d2585] w-screen h-screen">
+    <div className="bg-gradient-to-b from-[#121031] from-50% to-[#3d2585] w-screen min-h-screen">
       <div className="h-100 absolute -top-30 -left-10 w-150 bg-blue-900/50 rounded-full blur-3xl z-0"></div>
       {isLanding ? <Landing setIsLanding={setIsLanding} /> : <PromptPage />}
     </div>
@@ -111,40 +107,46 @@ function PromptPage() {
 
   return (
     <PageDiv>
-      <Logo logoAnimate={logoAnimate} img="logo2.png" width="w-80" pt="pt-10" />
+      <Logo
+        logoAnimate={logoAnimate}
+        img="logo2.png"
+        mdwidth="md:w-80"
+        width="w-60"
+        pt="pt-10"
+      />
 
       <Input></Input>
     </PageDiv>
   );
 }
 
-function Logo({ logoAnimate, img, width = "w-200", pt = "pt-50" }) {
+function Logo({
+  logoAnimate,
+  img,
+  mdwidth = "md:w-200",
+  width = "w-100",
+  pt = "pt-50",
+}) {
   return (
-    <div className={pt}>
+    <div className={`${pt}`}>
       <img
         src={`/imgs/${img}`}
-        className={`${width} mx-auto animate__animated animate__slower ${logoAnimate}`}
+        className={`  ${mdwidth} ${width} mx-auto animate__animated animate__slower ${logoAnimate} transition-all`}
       />
     </div>
   );
 }
 
-function Button({
-  isLogoDone,
-  onButtonClick,
-  textSize = "text-3xl",
-  mt = "mt-20",
-  children,
-}) {
+function Button({ isLogoDone, onButtonClick, mt = "mt-20", text="text-xl md:text-3xl", children }) {
   return (
     <div
       className={`animate__animated animate__slower  ${
         isLogoDone ? "animate__fadeIn" : "invisible"
-      }  w-fit mx-auto border-4 border-[#FAD776] hover:scale-105 transition-all duration-300 hover:bg-[#d2b158] group ${mt}  rounded-full p-1.5 shadow-lg shadow-[#F4EA90]/30`}
+      }  w-fit mx-auto border-4 border-[#FAD776] hover:scale-105 transition-all duration-300 hover:bg-[#d2b158] group ${mt}  rounded-full md:p-1.5 p-1 shadow-lg shadow-[#F4EA90]/30`}
     >
       <button
         onClick={onButtonClick}
-        className={` text-[#F4EA90] px-5 py-1 pb-3 cursor-pointer  rounded-full ${textSize} bg-[#F4EA90]/20 tracking-wide group-hover:text-white transition-all`}
+        className={`text-[#F4EA90] px-5 py-1 md:pb-3 pb-2 cursor-pointer rounded-full ${text} bg-[#F4EA90]/20 tracking-wide group-hover:text-white transition-all`}
       >
         {children}
       </button>
@@ -164,7 +166,6 @@ function Input({ children }) {
   const [card3, setCard3] = useState("");
 
   const [cardClicked, setCardClicked] = useState(false);
-
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -202,10 +203,13 @@ function Input({ children }) {
   }
 
   return (
-    <form className="flex flex-col mt-15" onSubmit={handleSubmit}>
-      <label className="z-20 text-3xl tracking-wide text-center text-[#9EB2FF] animate__animated animate__fadeIn animate__delay-1s">
+    <form
+      className="flex flex-col md:mt-15 mt-10 transition-all"
+      onSubmit={handleSubmit}
+    >
+      <label className="z-20 md:text-3xl text-lg tracking-wide text-center text-[#9EB2FF] animate__animated animate__fadeIn animate__delay-1s transition-all">
         <span className=" text-shadow-lg text-shadow-[#9EB2FF]/20 flex gap-2 w-fit mx-auto">
-          <img src="imgs/star.png" className="w-12" />
+          <img src="imgs/star.png" className="md:w-12 w-10" />
           What is your qwuestion for{" "}
           <span className="text-[#CEA1FF] text-shadow-lg text-shadow-[#CEA1FF]/20">
             oracle orpheus?
@@ -213,7 +217,7 @@ function Input({ children }) {
         </span>
       </label>
       <input
-        className="focus:outline-0 active:outline-0 focus:bg-[#CEA1FF]/15 focus:outline-[#d2b158] transition-all animate__animated animate__fadeIn animate__delay-2s z-20  text-2xl text-center w-220 mx-auto border-double border-[#9EB2FF]/50 bg-[#9EB2FF]/10 rounded-2xl h-18 mt-10"
+        className="focus:outline-0 active:outline-0 focus:bg-[#CEA1FF]/15 focus:outline-[#d2b158] transition-all animate__animated animate__fadeIn animate__delay-2s z-20  md:text-2xl text-lg text-center md:w-220 w-3/4 mx-auto border-double border-[#9EB2FF]/50 bg-[#9EB2FF]/10 rounded-2xl md:h-18 h-14 md:mt-10 mt-5"
         type="text"
         value={question}
         onChange={(e) => {
@@ -222,11 +226,7 @@ function Input({ children }) {
         }}
       ></input>
 
-      <Button
-        isLogoDone={isShowButton}
-        textSize="text-xl"
-        mt="mt-14"
-      >
+      <Button isLogoDone={isShowButton} text="text-base md:text-2xl" mt="md:mt-14 mt-12">
         Draw Cards
       </Button>
       <Orpheus
@@ -237,12 +237,16 @@ function Input({ children }) {
         setCardClicked={setCardClicked}
       />
       <p
-  className={`mx-auto w-220 text-center text-base mt-15 animate__animated mb-15 ${
-    isFadeOut ? "animate__fadeOut" : isFadeIn ? "animate__fadeIn animate__delay-5s" : ""
-  }`}
->
-  {response}
-</p>
+        className={`mx-auto md:w-220 px-2 text-center md:text-base text-sm md:mt-15 mt-5 animate__animated mb-15 ${
+          isFadeOut
+            ? "animate__fadeOut"
+            : isFadeIn
+            ? "animate__fadeIn animate__delay-5s"
+            : ""
+        }`}
+      >
+        {response}
+      </p>
     </form>
   );
 }
@@ -278,25 +282,27 @@ function Orpheus({ card1, card2, card3, cardClicked, setCardClicked }) {
     }
   }, [cardClicked, setCardClicked, card1, card2, card3]);
   return (
-    <div className="flex w-fit mx-auto">
+    <div className="flex md:flex-row flex-col w-fit mx-auto mt-10">
       <img
         src="imgs/orpheus.png"
-        className={`h-60 mx-auto mt-20 animate__animated  `}
+        className={`md:h-60 h-25 mx-auto animate__animated  `}
       />
-      <Card
-        card={c1 !== "" ? c1 : { number: "cover" }}
-        cardClicked={cardClicked}
-      />
-      <Card
-        card={c2 !== "" ? c2 : { number: "cover" }}
-        cardClicked={cardClicked}
-        delay="animate__delay-1s"
-      />
-      <Card
-        card={c3 !== "" ? c3 : { number: "cover" }}
-        cardClicked={cardClicked}
-        delay="animate__delay-2s"
-      />
+      <div className="flex w-fit mx-auto">
+        <Card
+          card={c1 !== "" ? c1 : { number: "cover" }}
+          cardClicked={cardClicked}
+        />
+        <Card
+          card={c2 !== "" ? c2 : { number: "cover" }}
+          cardClicked={cardClicked}
+          delay="animate__delay-1s"
+        />
+        <Card
+          card={c3 !== "" ? c3 : { number: "cover" }}
+          cardClicked={cardClicked}
+          delay="animate__delay-2s"
+        />
+      </div>
     </div>
   );
 }
@@ -305,7 +311,7 @@ function Card({ card = { number: "cover" }, cardClicked, delay = "" }) {
   return (
     <img
       src={`imgs/${card.number}.png`}
-      className={`h-60 mx-auto mt-15 animate__animated  ${
+      className={`md:h-60 h-40 mx-auto animate__animated  ${
         cardClicked ? "animate__fadeOut" : "animate__fadeIn"
       } ${delay} `}
     />
